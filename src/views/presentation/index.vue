7dc221deef14e366c352a96209e036ab08689c83
lebowska <lebow_ska@hotmail.it>
2023-03-25 20:32:38 +0100

    fix: merge gone wrong

--- src/views/presentation/index.vue
+++ src/views/presentation/index.vue
@@ -1,24 +1,15 @@
 <template>
-<<<<<<< HEAD
     <div class="w-full h-full" style="background: #b1cdc4;">
 
         <section class="relative w-full lg:h-screen">
-=======
-    <div class="w-full h-full">
-
-        <section class="relative w-full h-screen">
->>>>>>> ff0feb973d85e3e60524c9e9c4da834d49fab3d3
             <who />
         </section>
 
         <section class="relative">
-<<<<<<< HEAD
             <aboutMe />
         </section>
 
         <section class="relative">
-=======
->>>>>>> ff0feb973d85e3e60524c9e9c4da834d49fab3d3
             <what />
         </section>
 
@@ -34,19 +25,12 @@
 </template>
 <script setup lang="ts">
 import who from './sections/who.vue';
-<<<<<<< HEAD
 import aboutMe from './sections/aboutMe.vue';
-=======
->>>>>>> ff0feb973d85e3e60524c9e9c4da834d49fab3d3
 import what from './sections/what.vue';
 import points from './sections/points.vue';
 import footers from './sections/footers.vue';
 </script>
 
-<<<<<<< HEAD
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300;400;500;600;700&display=swap');
-=======
-<style scoped lang="scss">
->>>>>>> ff0feb973d85e3e60524c9e9c4da834d49fab3d3
 </style>
